ee8006cfb22c01041b5c770cc606cdf4e99bf318
Marc1515 <meq.1515@gmail.com>
2024-01-30 10:09:39 +1100

    It was only to test the Google user Button

--- app/(platform)/protected/page.tsx
+++
@@ -1,9 +0,0 @@
-import { currentUser } from "@clerk/nextjs";
-
-const ProtectedPage = async () => {
-  const user = await currentUser();
-
-  return <div>User: {user?.firstName}</div>;
-};
-
-export default ProtectedPage;
