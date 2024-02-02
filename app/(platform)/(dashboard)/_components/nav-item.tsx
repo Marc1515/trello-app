92878a5529a7c6e48689d53db89b6af361914ca7
Marc1515 <meq.1515@gmail.com>
2024-02-02 11:36:56 +1100

    I've setted the Accordion trigger with its expand event and dynamic classes, after I've setted the image for each nav-item

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,5 +1,10 @@
 "use-client";
 
+import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
 export type Organization = {
   id: string;
   slug: string;
@@ -20,5 +25,26 @@ export const NavItem = ({
   organization,
   onExpand,
 }: NavItemProps) => {
-  return <div>Nav Item!</div>;
+  return (
+    <AccordionItem value={organization.id} className="border-none">
+      <AccordionTrigger
+        onClick={() => onExpand(organization.id)}
+        className={cn(
+          "flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline",
+          isActive && !isExpanded && "bg-sky-500/10 text-sky-700"
+        )}
+      >
+        <div className="felx items-center gap-x-2">
+          <div className="w-7 h-7 relative">
+            <Image
+              fill
+              src={organization.imageUrl}
+              alt="Organization"
+              className="rounded-sm object-cover"
+            />
+          </div>
+        </div>
+      </AccordionTrigger>
+    </AccordionItem>
+  );
 };
