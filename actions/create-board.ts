15109829bb85f860729e7b7e4a86917ed764c74c
Marc1515 <meq.1515@gmail.com>
2024-02-05 08:12:55 +1100

    Actually, I don't understand at all what is going on...

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -4,21 +4,49 @@ import {z} from "zod";
 
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
-const createBoard =z.object({
-    title: z.string(),
+export type State = {
+  errors?: {
+    title?: string[];
+  },
+  message?: string | null;
+}
+
+const CreateBoard = z.object({
+    title: z.string().min(3, {
+      message: "Minimum length of 3 letters is required"
+    }),
 })
 
-export async function create(formData: FormData) {
+export async function create(prevState: State, formData: FormData) {
 
-    const {title} = createBoard.parse({
+    const validateFields = CreateBoard.safeParse({
         title: formData.get("title")
     });
 
-    await db.board.create({
-      data: {
-        title,
-      },
-    });
+    if(!validateFields.success) {
+      return {
+        errors: validateFields.error.flatten().fieldErrors,
+        messsage: "Missing Fields"
+      }
+    }
+
+    const {title} = validateFields.data;
+
+    try {
+      await db.board.create({
+        data: {
+          title,
+        },
+      });
+    } catch (error) {
+      return{      
+        message: "Database Error"
+      }
+    }
+
+
     revalidatePath("/organization/org_2bkLhXmlvUywmOGEoB9ZVkwyhD6");
+    redirect("/organization/org_2bkLhXmlvUywmOGEoB9ZVkwyhD6");
   }
