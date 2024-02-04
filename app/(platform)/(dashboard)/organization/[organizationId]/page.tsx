61594480007ec838b0189bff9f6f4d4a15586750
Marc1515 <meq.1515@gmail.com>
2024-02-05 06:53:03 +1100

    Now I've setted the OrganizationIdPage component as a async function because it's gonna recive the data from prisma and it've needed to be with async way

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,12 @@
 import { create } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
   return (
-    <div>
+    <div className="flex flex-col space-y-4">
       <form action={create}>
         <input
           id="title"
@@ -14,6 +17,11 @@ const OrganizationIdPage = () => {
         />
         <Button type="submit">Submit</Button>
       </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>Board name: {board.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
