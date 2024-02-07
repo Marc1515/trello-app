14fb044cd45089bd4e368c3eec308dcae121cb35
Marc1515 <meq.1515@gmail.com>
2024-02-07 20:47:43 +1100

    Just the import of the new FormInput component

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,10 +1,10 @@
 "use client";
 
+import { useAction } from "@/hooks/use-action";
 import { createBoard } from "@/actions/create-board";
 
-import { FormInput } from "./form-input";
 import { FormButton } from "./form-button";
-import { useAction } from "@/hooks/use-action";
+import { FormInput } from "@/components/form/form-input";
 
 export const Form = () => {
   const { execute, fieldErrors } = useAction(createBoard, {
@@ -26,7 +26,7 @@ export const Form = () => {
   return (
     <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={fieldErrors} />
+        <FormInput id="title" errors={fieldErrors} />
       </div>
       <FormButton />
     </form>
