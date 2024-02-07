65cc2c69a9ebfedf0875a72d143b600f0021a3b9
Marc1515 <meq.1515@gmail.com>
2024-02-07 15:39:40 +1100

    Now I'm gonna create the components with a better way, and I will start with the form input and its interface

---
+++ components/form/form-input.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { forwardRef } from "react";
+
+interface FormInputProps {
+  id: string;
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  errors?: Record<string, string[] | undefined>;
+  className?: string;
+  defaultValue?: string;
+  onBlur?: () => void;
+}
