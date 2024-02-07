fd12597355a99373b8724781fde8860de2713175
Marc1515 <meq.1515@gmail.com>
2024-02-07 20:48:43 +1100

    A litle mistake with the map solved

--- components/form/form-errors.tsx
+++ components/form/form-errors.tsx
@@ -14,7 +14,7 @@ export const FormErrors = ({ id, errors }: FormErrorsProps) => {
       aria-live="polite"
       className="mt-2 text-xs text-rose-500"
     >
-      {errors?.id?.map((error: string) => (
+      {errors?.[id]?.map((error: string) => (
         <div
           key={error}
           className="flex items-center font-medium p-2 border border-rose-500 bg-rose-500/10 rounded-sm"
