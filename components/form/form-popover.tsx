af806ea8a590781e89fcd957d36d18e02085b411
Marc1515 <meq.1515@gmail.com>
2024-02-08 12:27:07 +1100

    Now I've setted the errors and the data to control what I'm reciving for the popover component

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import { X } from "lucide-react";
+
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
+  PopoverClose,
 } from "@/components/ui/popover";
+
+import { Button } from "../ui/button";
 
 import { useAction } from "@/hooks/use-action";
 import { CreateBoard } from "@/actions/create-board/schema";
 
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
+import { createBoard } from "@/actions/create-board";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -25,6 +31,21 @@ export const FormPopover = ({
   align,
   sideOffSet = 0,
 }: FormPopoverProps) => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log({ data });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  };
+
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
@@ -37,6 +58,25 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create Board
         </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant={"ghost"}
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormInput
+              id="title"
+              label="Board title"
+              type="text"
+              errors={fieldErrors}
+            />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
       </PopoverContent>
     </Popover>
   );
