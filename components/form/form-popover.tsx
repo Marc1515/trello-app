4e16b601bfc1095c2c7cbb0e7f765d796289940e
Marc1515 <meq.1515@gmail.com>
2024-02-10 15:14:44 +1100

    I've setted that the popover has to close it when whe create the board

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { ElementRef, useRef } from "react";
+
 import { toast } from "sonner";
 
 import { X } from "lucide-react";
+
+import { useRouter } from "next/navigation";
 
 import {
   Popover,
@@ -32,10 +36,15 @@ export const FormPopover = ({
   align,
   sideOffSet = 0,
 }: FormPopoverProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);
+
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
       console.log({ data });
       toast.success("Board created!");
+      closeRef?.current?.click();
+      router.push(`/board/${data.id}`);
     },
     onError: (error) => {
       console.log(error);
@@ -62,7 +71,7 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create Board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant={"ghost"}
