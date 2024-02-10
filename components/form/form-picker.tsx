2cd9556cd5bbe4c6a9a05b0721f5c02f18f2380a
Marc1515 <meq.1515@gmail.com>
2024-02-10 11:55:19 +1100

    I've setted a map which will show all the images from unsplash with  3x3 grid template

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { unsplash } from "@/lib/unsplash";
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
 import { useState, useEffect } from "react";
+import { useFormStatus } from "react-dom";
+
+import { cn } from "@/lib/utils";
+import { unsplash } from "@/lib/unsplash";
 
 interface FormPickerPorps {
   id: string;
@@ -9,7 +14,11 @@ interface FormPickerPorps {
 }
 
 export const FormPicker = ({ id, errors }: FormPickerPorps) => {
+  const pending = useFormStatus();
+
   const [images, setImages] = useState<Array<Record<string, any>>>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedImageId, setSelectedImageId] = useState(null);
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -28,9 +37,45 @@ export const FormPicker = ({ id, errors }: FormPickerPorps) => {
       } catch (error) {
         console.log(error);
         setImages([]);
+      } finally {
+        setIsLoading(false);
       }
     };
+    fetchImages();
   }, []);
 
-  return <div>Form Picker!</div>;
+  if (isLoading) {
+    return (
+      <div className="p-6 flex items-center justify-center">
+        <Loader2 className="h-6 w-6 text-sky-700 animate-spin" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative">
+      <div className="grid grid-cols-3 gap-2 mb-2">
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className={cn(
+              "cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted",
+              pending && "opacity-50 hover:opacity-50 cursor-auto"
+            )}
+            onClick={() => {
+              if (pending) return;
+              setSelectedImageId(image.id);
+            }}
+          >
+            <Image
+              src={image.urls.thumb}
+              alt="unsplash image"
+              className="object-cover rounded-sm"
+              fill
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
