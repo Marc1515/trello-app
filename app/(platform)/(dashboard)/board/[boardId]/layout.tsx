add927c6ab6e09f7fee8d20ee3664b49c356dbd7
Marc1515 <meq.1515@gmail.com>
2024-02-11 21:39:19 +1100

    I'm using the layout to style the background with the board images

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,39 @@
+import { auth } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
+  return (
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default BoardIdLayout;
