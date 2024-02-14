4f2699d2da48ba0ad6799e959a01433848d4c2b7
Marc1515 <meq.1515@gmail.com>
2024-02-14 18:50:37 +1100

    The list-form component importation

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { List } from "@prisma/client";
+import { ListWithCards } from "@/types";
+
+import { ListForm } from "./list-form";
 
 interface ListContainerProps {
-  data: List[];
+  data: ListWithCards[];
   boardId: string;
 }
 
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
-  return <div>List container</div>;
+  return (
+    <ol>
+      <ListForm />
+      <div className="flex-shrink-0 w-1" />
+    </ol>
+  );
 };
