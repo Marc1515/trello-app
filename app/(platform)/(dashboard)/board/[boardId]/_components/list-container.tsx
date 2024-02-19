f6aa7d45b5f5dbd5ba346b210f4eebb631ba4aa5
Marc1515 <meq.1515@gmail.com>
2024-02-19 21:43:23 +1100

    I'm strugglying with the items but I cannot see the error, tomorrow I will fix it

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { toast } from "sonner";
 import { useEffect, useState } from "react";
 import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 
 import { ListWithCards } from "@/types";
+import { useAction } from "@/hooks/use-action";
+import { updateListOrder } from "@/actions/update-list-order";
+import { updateCardOrder } from "@/actions/update-card-order";
 
 import { ListForm } from "./list-form";
 import { ListItem } from "./list-item";
@@ -24,6 +28,24 @@ function reorder<T>(list: T[], startIndex: number, endIndex: number) {
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   const [orderedData, setOrderedData] = useState(data);
 
+  const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
+    onSuccess: () => {
+      toast.success("List reordered");
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const { execute: executeUpdateCardOrder } = useAction(updateCardOrder, {
+    onSuccess: () => {
+      toast.success("Card reordered");
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   useEffect(() => {
     setOrderedData(data);
   }, [data]);
@@ -35,8 +57,7 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
       return;
     }
 
-    // If dropped in the same position
-
+    // if dropped in the same position
     if (
       destination.droppableId === source.droppableId &&
       destination.index === source.index
@@ -49,7 +70,9 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
       const items = reorder(orderedData, source.index, destination.index).map(
         (item, index) => ({ ...item, order: index })
       );
+
       setOrderedData(items);
+      executeUpdateListOrder({ items, boardId });
     }
 
     // User moves a card
@@ -58,22 +81,22 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
 
       // Source and destination list
       const sourceList = newOrderedData.find(
-        (list) => list.id === source.draggableId
+        (list) => list.id === source.droppableId
       );
       const destList = newOrderedData.find(
-        (list) => list.id === destination.draggableId
+        (list) => list.id === destination.droppableId
       );
 
-      if (!sourceList || !destList) return;
+      if (!sourceList || !destList) {
+        return;
+      }
 
-      // Check if cards exist on the sourceList
-
+      // Check if cards exists on the sourceList
       if (!sourceList.cards) {
         sourceList.cards = [];
       }
 
       // Check if cards exists on the destList
-
       if (!destList.cards) {
         destList.cards = [];
       }
@@ -93,6 +116,35 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
         sourceList.cards = reorderedCards;
 
         setOrderedData(newOrderedData);
+        executeUpdateCardOrder({
+          boardId: boardId,
+          items: reorderedCards,
+        });
+        // User moves the card to another list
+      } else {
+        // Remove card from the source list
+        const [movedCard] = sourceList.cards.splice(source.index, 1);
+
+        // Assign the new listId to the moved card
+        movedCard.listId = destination.droppableId;
+
+        // Add card to the destination list
+        destList.cards.splice(destination.index, 0, movedCard);
+
+        sourceList.cards.forEach((card, idx) => {
+          card.order = idx;
+        });
+
+        // Update the order for each card in the destination list
+        destList.cards.forEach((card, idx) => {
+          card.order = idx;
+        });
+
+        setOrderedData(newOrderedData);
+        executeUpdateCardOrder({
+          boardId: boardId,
+          items: destList.cards,
+        });
       }
     }
   };
