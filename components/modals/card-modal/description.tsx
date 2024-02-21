ada7e5ac2f14046cc950d52bdff45c7417504921
Marc1515 <meq.1515@gmail.com>
2024-02-22 10:32:52 +1100

    Description component from modal component finished

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import { toast } from "sonner";
 import { AlignLeft } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useState, useRef, ElementRef } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useEventListener, useOnClickOutside } from "usehooks-ts";
 
+import { useAction } from "@/hooks/use-action";
+import { updateCard } from "@/actions/update-card";
 import { CardWithList } from "@/types";
 import { Skeleton } from "@/components/ui/skeleton";
 import { FormTextarea } from "@/components/form/form-textarea";
 import { FormSubmit } from "@/components/form/form-submit";
 import { Button } from "@/components/ui/button";
+import { error } from "console";
 
 interface DescriptionProps {
   data: CardWithList;
@@ -45,9 +49,28 @@ export const Description = ({ data }: DescriptionProps) => {
   useEventListener("keydown", onKeyDown);
   useOnClickOutside(formRef, disableEditing);
 
+  const { execute, fieldErrors } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["card", data.id],
+      });
+      toast.success(`Card "${data.title}" updated`);
+      disableEditing();
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
   const onSubmit = (formData: FormData) => {
-    const description = formData.get("description");
+    const description = formData.get("description") as string;
     const boardId = params.boardId as string;
+
+    execute({
+      id: data.id,
+      description,
+      boardId,
+    });
   };
 
   return (
@@ -56,12 +79,14 @@ export const Description = ({ data }: DescriptionProps) => {
       <div className="w-full">
         <p className="font-semibold text-neutral-700 mb-2">Description</p>
         {isEditing ? (
-          <form ref={formRef} className="space-y-2">
+          <form action={onSubmit} ref={formRef} className="space-y-2">
             <FormTextarea
               id="description"
               className="w-full mt-2"
               placeholder="Add a more detailed description"
               defaultValue={data.description || undefined}
+              errors={fieldErrors}
+              ref={textareaRef}
             />
             <div className="flex items-center gap-x-2">
               <FormSubmit>Save</FormSubmit>
