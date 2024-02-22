4c4ab55e4842f7e8e7937f5cd89ba91ab61d865b
Marc1515 <meq.1515@gmail.com>
2024-02-23 07:08:50 +1100

    I've setted the createAuditLog to register every time some user createa new card

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -2,8 +2,10 @@
 
 import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 import { db } from "@/lib/db";
+import { CreateAuditLog } from "@/lib/create-audit-log";
 import { createSafeAction } from "@/lib/create-safe-action";
 
 import { CreateCard } from "./schema";
@@ -49,6 +51,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         listId,
         order: newOrder,
       },
+    });
+
+    await CreateAuditLog({
+      entityId: card.id,
+      entityTitle: card.title,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.CREATE,
     });
   } catch (error) {
     return {
