b35418fa9284f9e288cb0d70c6f4e89fea71bce8
Marc1515 <meq.1515@gmail.com>
2024-02-24 10:07:49 +1100

    Before download the date's package now I can format to my preference

--- components/activity-item.tsx
+++ components/activity-item.tsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import { AuditLog } from "@prisma/client";
 
 import { generateLogMessage } from "@/lib/generate-log-message";
@@ -17,7 +18,11 @@ export const ActivityItem = ({ data }: ActivityItemProps) => {
         <p className="text-sm text-muted-foreground">
           <span className="font-semibold lowercase text-neutral-700">
             {data.userName}
-          </span> {generateLogMessage(data)}
+          </span>{" "}
+          {generateLogMessage(data)}
+        </p>
+        <p className="text-xs text-muted-foreground">
+          {format(new Date(data.createdAt), "MMM d, yyyy 'at' h:mm a")}
         </p>
       </div>
     </li>
