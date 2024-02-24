46f576f3b003d91f3ee5903d618f16f33ce2abd3
Marc1515 <meq.1515@gmail.com>
2024-02-25 10:19:45 +1100

    The rules to decide what the user can do is on this file, org-limits.ts

--- lib/org-limit.ts
+++ lib/org-limit.ts
@@ -45,3 +45,33 @@ export const decreaseAvaibleCount = async () => {
     });
   }
 };
+
+export const hasAvailableCount = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) throw new Error("Unauthorized");
+
+  const orgLimit = await db.orgLimit.findUnique({
+    where: { orgId },
+  });
+
+  if (!orgLimit || orgLimit.count < MAX_FREE_BOARDS) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const getAvailableCount = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) return 0;
+
+  const orgLimit = await db.orgLimit.findUnique({
+    where: { orgId },
+  });
+
+  if (!orgLimit) return 0;
+
+  return orgLimit.count;
+};
