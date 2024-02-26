78e28feb04790d3cdee0baa04cf2654e9ca74d3c
Marc1515 <meq.1515@gmail.com>
2024-02-26 21:42:07 +1100

    I'm creating the hook achive the user payment

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,6 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+import { stripe } from "@/lib/stripe";
