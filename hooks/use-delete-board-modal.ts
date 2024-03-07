8505fefece235de00c3193cfa0b9ba3f38de5cf8
Marc1515 <meq.1515@gmail.com>
2024-03-08 10:39:47 +1100

    It's necessary to set the new value in the hook

--- hooks/use-delete-board-modal.ts
+++ hooks/use-delete-board-modal.ts
@@ -2,14 +2,16 @@ import { create } from "zustand";
 
 type DeleteBoardModalStore = {
   id?: string;
+  title?: string;
   isOpen: boolean;
-  onOpen: (id: string) => void;
+  onOpen: (id: string, title: string) => void;
   onClose: () => void;
 };
 
 export const useDeleteBoardModal = create<DeleteBoardModalStore>((set) => ({
   id: undefined,
+  title: undefined,
   isOpen: false,
-  onOpen: (id: string) => set({ isOpen: true, id }),
+  onOpen: (id: string, title: string) => set({ isOpen: true, id, title }),
   onClose: () => set({ isOpen: false, id: undefined }),
 }));
