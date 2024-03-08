618a63b2757379bc4e4145e5e273e2786042b153
Marc1515 <meq.1515@gmail.com>
2024-03-09 09:46:30 +1100

    It was necessary to pass all the data cause is the required value now

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -12,7 +12,7 @@ export const BoardNavbar = async ({ data }: BoardNavbarProps) => {
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
       <BoardTitleForm data={data} />
       <div className="ml-auto">
-        <BoardOptions id={data.id} />
+        <BoardOptions data={data} />
       </div>
     </div>
   );
