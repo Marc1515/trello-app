b8cfebda88d0f4c40c49f3e37836b8059a2d99b6
Marc1515 <meq.1515@gmail.com>
2024-03-10 23:34:02 +1100

    This file will content the data for the 3 default boards when the user create a new organization

---
+++ constants/defaultBoardImages.ts
@@ -0,0 +1,114 @@
+export const defaultBoardImages = [
+  {
+    id: "Hd7vwFzZpH0",
+    slug: "cat-licking-its-paw-Hd7vwFzZpH0",
+    alternative_slugs: {
+      en: "cat-licking-its-paw-Hd7vwFzZpH0",
+    },
+    created_at: "2017-11-21T14:49:18Z",
+    updated_at: "2024-03-09T16:14:07Z",
+    promoted_at: "2017-11-21T15:48:52Z",
+    width: 6321,
+    height: 4214,
+    color: "#737373",
+    blur_hash: "LBC6_o^,4mRi?ckBRPs;4,ITnk%M",
+    description: null,
+    alt_description: "cat licking its paw",
+    breadcrumbs: [],
+    urls: {
+      raw: "https://images.unsplash.com/photo-1511275539165-cc46b1ee89bf?ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDA3MjF8\u0026ixlib=rb-4.0.3",
+      full: "https://images.unsplash.com/photo-1511275539165-cc46b1ee89bf?crop=entropy\u0026cs=srgb\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDA3MjF8\u0026ixlib=rb-4.0.3\u0026q=85",
+      regular:
+        "https://images.unsplash.com/photo-1511275539165-cc46b1ee89bf?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDA3MjF8\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=1080",
+      small:
+        "https://images.unsplash.com/photo-1511275539165-cc46b1ee89bf?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDA3MjF8\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=400",
+      thumb:
+        "https://images.unsplash.com/photo-1511275539165-cc46b1ee89bf?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDA3MjF8\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=200",
+      small_s3:
+        "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1511275539165-cc46b1ee89bf",
+    },
+    links: {
+      self: "https://api.unsplash.com/photos/cat-licking-its-paw-Hd7vwFzZpH0",
+      html: "https://unsplash.com/photos/cat-licking-its-paw-Hd7vwFzZpH0",
+      download:
+        "https://unsplash.com/photos/Hd7vwFzZpH0/download?ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDA3MjF8",
+      download_location:
+        "https://api.unsplash.com/photos/Hd7vwFzZpH0/download?ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDA3MjF8",
+    },
+  },
+  {
+    id: "4Qd2KkJCXOk",
+    slug: "red-rose-with-black-background-4Qd2KkJCXOk",
+    alternative_slugs: {
+      en: "red-rose-with-black-background-4Qd2KkJCXOk",
+    },
+    created_at: "2018-07-27T23:12:55Z",
+    updated_at: "2024-03-09T11:05:28Z",
+    promoted_at: "2018-07-28T12:30:09Z",
+    width: 4912,
+    height: 3264,
+    color: "#260c26",
+    blur_hash: "L55|ECa}2rfRsCa|Sfo1J,jtwda|",
+    description:
+      "I took this photo a while back and only recently ran it through post. I am very happy with the results.",
+    alt_description: "red rose with black background",
+    breadcrumbs: [],
+    urls: {
+      raw: "https://images.unsplash.com/photo-1532732789821-e69c5633391e?ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDEzNjB8\u0026ixlib=rb-4.0.3",
+      full: "https://images.unsplash.com/photo-1532732789821-e69c5633391e?crop=entropy\u0026cs=srgb\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDEzNjB8\u0026ixlib=rb-4.0.3\u0026q=85",
+      regular:
+        "https://images.unsplash.com/photo-1532732789821-e69c5633391e?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDEzNjB8\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=1080",
+      small:
+        "https://images.unsplash.com/photo-1532732789821-e69c5633391e?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDEzNjB8\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=400",
+      thumb:
+        "https://images.unsplash.com/photo-1532732789821-e69c5633391e?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDEzNjB8\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=200",
+      small_s3:
+        "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1532732789821-e69c5633391e",
+    },
+    links: {
+      self: "https://api.unsplash.com/photos/red-rose-with-black-background-4Qd2KkJCXOk",
+      html: "https://unsplash.com/photos/red-rose-with-black-background-4Qd2KkJCXOk",
+      download:
+        "https://unsplash.com/photos/4Qd2KkJCXOk/download?ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDEzNjB8",
+      download_location:
+        "https://api.unsplash.com/photos/4Qd2KkJCXOk/download?ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDEzNjB8",
+    },
+  },
+  {
+    id: "4XXNWJJ4msg",
+    slug: "blue-ocean-4XXNWJJ4msg",
+    alternative_slugs: {
+      en: "blue-ocean-4XXNWJJ4msg",
+    },
+    created_at: "2018-12-11T17:10:40Z",
+    updated_at: "2024-03-09T10:07:00Z",
+    promoted_at: "2018-12-12T11:26:27Z",
+    width: 5464,
+    height: 3640,
+    color: "#262626",
+    blur_hash: "LMFP4}4.=|xD_}S5$$xZMc%MjExF",
+    description: null,
+    alt_description: "blue ocean",
+    breadcrumbs: [],
+    urls: {
+      raw: "https://images.unsplash.com/photo-1544548188-597e0d9e50bf?ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDE4ODl8\u0026ixlib=rb-4.0.3",
+      full: "https://images.unsplash.com/photo-1544548188-597e0d9e50bf?crop=entropy\u0026cs=srgb\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDE4ODl8\u0026ixlib=rb-4.0.3\u0026q=85",
+      regular:
+        "https://images.unsplash.com/photo-1544548188-597e0d9e50bf?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDE4ODl8\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=1080",
+      small:
+        "https://images.unsplash.com/photo-1544548188-597e0d9e50bf?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDE4ODl8\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=400",
+      thumb:
+        "https://images.unsplash.com/photo-1544548188-597e0d9e50bf?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDE4ODl8\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=200",
+      small_s3:
+        "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1544548188-597e0d9e50bf",
+    },
+    links: {
+      self: "https://api.unsplash.com/photos/blue-ocean-4XXNWJJ4msg",
+      html: "https://unsplash.com/photos/blue-ocean-4XXNWJJ4msg",
+      download:
+        "https://unsplash.com/photos/4XXNWJJ4msg/download?ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDE4ODl8",
+      download_location:
+        "https://api.unsplash.com/photos/4XXNWJJ4msg/download?ixid=M3w1NjQ2Mjl8MHwxfGFsbHx8fHx8fHx8fDE3MTAwNDE4ODl8",
+    },
+  },
+];
