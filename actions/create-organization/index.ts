1e00f586858b342b87a8c6f920622fbe4bb34256
Marc1515 <meq.1515@gmail.com>
2024-03-16 17:00:50 +1100

    At this moment the default boards are created but I need to split the code

--- actions/create-organization/index.ts
+++ actions/create-organization/index.ts
@@ -2,10 +2,27 @@
 
 import { auth } from "@clerk/nextjs";
 import { db } from "@/lib/db";
+import { defaultBoardImages } from "@/constants/defaultBoardImages";
 
 type ReturnType = {
   error?: string;
   data?: any;
+};
+
+const createDefaultBoards = async (orgId: string) => {
+  for (const boardImage of defaultBoardImages) {
+    await db.board.create({
+      data: {
+        orgId: orgId,
+        title: boardImage.alt_description || "Default Board",
+        imageId: boardImage.id,
+        imageThumbUrl: boardImage.urls.thumb,
+        imageFullUrl: boardImage.urls.full,
+        imageUserName: "Default",
+        imageLinkHTML: boardImage.links.html,
+      },
+    });
+  }
 };
 
 const createOrganization = async (): Promise<ReturnType> => {
@@ -15,27 +32,32 @@ const createOrganization = async (): Promise<ReturnType> => {
     return { error: "Unauthorized" };
   }
 
-  const existingOrganization = await db.organization.findUnique({
+  let organization = await db.organization.findUnique({
     where: { id: orgId },
   });
 
-  if (existingOrganization) {
-    return { error: "Organization already exists" };
-  }
-
-  try {
-    const organization = await db.organization.create({
+  if (!organization) {
+    organization = await db.organization.create({
       data: {
         id: orgId,
         name: "Nombre por defecto",
-        defaultBoardsCreated: false,
+        defaultBoardsCreated: false, // Asegura que esta propiedad esté en false inicialmente
       },
     });
 
+    // Crea los tableros por defecto solo si es la primera vez que se crea la organización
+    if (!organization.defaultBoardsCreated) {
+      await createDefaultBoards(orgId);
+      // Actualiza la organización para marcar que los tableros por defecto han sido creados
+      await db.organization.update({
+        where: { id: orgId },
+        data: { defaultBoardsCreated: true },
+      });
+    }
+
     return { data: organization };
-  } catch (error) {
-    console.error("Error al crear la organización:", error);
-    return { error: "Failed to create organization" };
+  } else {
+    return { error: "Organization already exists" };
   }
 };
 
