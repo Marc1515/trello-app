535ff989026380b04604c6d6e396777609a68091
Marc1515 <meq.1515@gmail.com>
2024-03-17 17:02:35 +1100

    I just deleted some comments and I will put a better ones

--- lib/get-clerk-organizations.ts
+++ lib/get-clerk-organizations.ts
@@ -1,13 +1,12 @@
 import { clerkClient } from "@clerk/nextjs";
 
 export async function getClerkOrganizations() {
-    try {
-      // Directamente devuelve un arreglo de organizaciones
-      const organizationsList =
-        await clerkClient.organizations.getOrganizationList();
-      return organizationsList.map((org) => org.id);
-    } catch (error) {
-      console.error("Error fetching organizations from Clerk:", error);
-      return []; // Retorna un arreglo vacío en caso de error
-    }
+  try {
+    const organizationsList =
+      await clerkClient.organizations.getOrganizationList();
+    return organizationsList.map((org) => org.id);
+  } catch (error) {
+    console.error("Error fetching organizations from Clerk:", error);
+    return [];
   }
+}
